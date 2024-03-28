b531d6cff1222bb485705a807fdd4ae8fa634c02
Gavrila Ivolgin <Nebosst@yandex.ru>
2024-03-28 19:58:49 +0200

    add custom filter censor

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django import template
+import re
+import string
 
 register = template.Library()
 
@@ -12,7 +14,33 @@ def censor(text):
     :param text: проверяемый объект <str>
     :return: объект <str> с замененным текстом "*"
     """
-    list =
+    filename = "dict_words.txt"
+    text_user = str(text).split()  # ['новость', 'об', 'хорошей', 'погоде']
 
-    print(str(text).lower())
-    return str(text).lower()
+    try:
+        with open(filename, "r", encoding="utf-8", errors="ignor") as file:
+
+            list_words = file.read().lower().split()  # <class 'str'> # ['отличный', 'передовой', ... 'хорошей']
+
+            censor_text = []
+
+            if text_user:
+                for word in text_user:
+                    # Убираем из проверяемого слова знаки препинания
+                    word_clear = re.sub(r'[^\w\s]', '', word)
+
+                    if word_clear.lower() in list_words:
+                        censor_word = word_clear[:1] + str(len(word_clear[1:]) * '*')
+                        censor_text.append(censor_word)
+                    else:
+                        censor_text.append(word)
+
+            return " ".join(censor_text)
+
+    except Exception as e:
+        print(e)
+
+
+
+
+
