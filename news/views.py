7f4a04b0329f16256a9ab45fe0bdd34422f8cf20
Gavrila Ivolgin <Nebosst@yandex.ru>
2024-03-30 14:55:11 +0200

    add comment in news/models

--- news/views.py
+++ news/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
 from news.models import Post
 
 
 class NewsList(ListView):
-    model = Post
+    model = Post  # queryset = Post.objects.all()
     ordering = "-dateCreation"
     template_name = "news/news.html"
     context_object_name = "news"
 
 
 class NewDetail(DetailView):
-    model = Post
+    model = Post  # queryset = Post.objects.get(pk=pk)
     template_name = "news/new.html"
     context_object_name = "new"
